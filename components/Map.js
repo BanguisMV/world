eb600c1d2ba2b0f7d5e53865a259fdc576f91323
BanguisMV <markvergelbanguis@gmail.com>
2020-11-22 21:41:38 -0800

    ..

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,19 @@
 import { MapContainer, TileLayer,Marker, Popup } from 'react-leaflet'
+import useMedia from 'use-media';
+
 //https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png
 //https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}
 //https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}
 const Map = () => {
+  const mobile = useMedia({maxWidth: 500});
+
     const position = [9.7571, 125.5138]
       return (
         <MapContainer 
         style={{ height: '100vh', width: '100%' }}
         minZoom={3}
         center={position} 
-        zoom={6} 
+        zoom={mobile ? 8 : 6} 
         scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic'
-
+import {NextSeo} from 'next-seo';
 const Map = dynamic(
   () => import('../components/Map'),
   { ssr: false }
@@ -9,6 +9,7 @@ const Home = () => {
 
   return (
    <div style={{ height: '100vh', width: '100%' }}>
+     <NextSeo  title="World" />
                 <Map />
     </div>
   )
