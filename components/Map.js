e0193f5643423139f457702e41bc896bb44ea26e
BanguisMV <markvergelbanguis@gmail.com>
2020-11-23 21:39:08 -0800

    ...

--- components/Map.js
+++ components/Map.js
@@ -5,31 +5,33 @@ import useMedia from 'use-media';
 const inactive = { color: '#141412' }
 const active = { color: '#d81111' }
 const earthquakesColor = { color: ' #ffd900' }
+const earthquakesColor2 = { color: '  #6ce41c' }
 
 const Map = ({ volcanoes, earthquakes }) => {
-  console.log(earthquakes.features);
 
+ 
     const mobile = useMedia({maxWidth: 500});
     const position = [12.8797, 121.7740]
       return (
         <Fragment>
-
           <div className='legends'>
-            
             <div className='total legend'>
                 <p>Approximately</p>
-                {/* <h2>{volcanoes.features.length}</h2> */}
+                <h2>{volcanoes.features.length}</h2>
                 <p> Volcanoes in Total</p>
             </div>
-
             <div className='active legend'>
                 <h2>Active</h2>
             </div>
-
             <div className='inactive legend'>
                 <h2>inactive</h2>
             </div>
-
+            <div className='up magnitude'>
+                <p>Magnitude 2 and up</p>
+            </div>
+            <div className='below magnitude'>
+                <p>Magnitude 2 and below</p>
+            </div>
           </div>
 
         <MapContainer 
@@ -44,36 +46,43 @@ const Map = ({ volcanoes, earthquakes }) => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-          {earthquakes.features.map(feature => (
+
+        
+
+          {earthquakes.features.map(earthquake => (
             <CircleMarker 
-              key={feature.id}
-              center={[feature.geometry.coordinates.[1], feature.geometry.coordinates.[0]]} 
-              pathOptions={feature.properties.mag > 1 ? earthquakesColor : inactive} 
-              radius={4}>
-            <Popup className='pop_up'> 
-               <div className='pop_child'>
-                <p>Location: {feature.properties.place}</p>
-                <p>Date: {new Date(feature.properties.time).toDateString()}</p>
-                <p>Magnitude: {new Date(feature.properties.mag)}</p>
+              key={earthquake.id}
+              center={[ earthquake.geometry.coordinates.[1], earthquake.geometry.coordinates.[0] ]} 
+              pathOptions={earthquake.properties.mag > 2 ? earthquakesColor : earthquakesColor2} 
+              radius={6}>
+
+            <Popup> 
+               <div>
+                <p>Location: {earthquake.properties.place}</p>
+                <p>Date: {new Date(earthquake.properties.time).toDateString()}</p>
+                <p>Magnitude: {earthquake.properties.mag}</p>
                </div>
               </Popup> 
             </CircleMarker>
         ))}
+
         {volcanoes.features.map(feature => (
             <CircleMarker 
               key={feature.properties.VolcanoID}
               center={[feature.properties.Latitude, feature.properties.Longitude]} 
               pathOptions={feature.properties.H_active === 1 ? active : inactive} 
               radius={mobile ? 5 : 4}>
+
             <Popup className='pop_up'>
               <div className='pop_child'>
-                <h1 className='pop_h1' > {feature.properties.V_Name} Volcano </h1>
+                <h4 className='pop_h1' > {feature.properties.V_Name} Volcano </h4>
                 <p className='pop_p' > Status: {feature.properties.H_active === 1  ? 'Active' : 'Inactive'}</p>
                 <p className='pop_p' > Erupted: {feature.properties.VEI_Holoce} </p>
               </div>
               </Popup>
             </CircleMarker>
         ))}
+
       </MapContainer>
       </Fragment>
       )
